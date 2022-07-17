f25286226bcb949fd7c835e92063d88e40af8764
TrucGiang <dohoangtrucgiang8338@gmail.com>
2022-07-17 21:41:33 +0700

    change some

--- src/layouts/dashboard/NavConfig.js
+++ src/layouts/dashboard/NavConfig.js
@@ -1,9 +1,12 @@
 // component
 import Iconify from '../../components/Iconify';
+import {listIcons} from "@iconify/react/dist/iconify";
 
 // ----------------------------------------------------------------------
 
 const getIcon = (name) => <Iconify icon={name} width={22} height={22} />;
+
+console.log(listIcons());
 
 const navConfig = [
   {
