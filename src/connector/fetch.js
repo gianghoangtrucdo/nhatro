36abe40f63468f2f2b918980055496717e967424
TrucGiang <dohoangtrucgiang8338@gmail.com>
2022-07-22 01:27:21 +0700

    handle login fail

--- src/connector/fetch.js
+++ src/connector/fetch.js
@@ -39,10 +39,6 @@ export const getRoomsByDomId = async (offset = 0, limit = 50, domId) => {
     })
 }
 
-export const createContract = async (body) => {
-    return await post(consts.createContract, body)
-}
-
 export const getContracts = async (offset = 0, limit = 50) => {
     return await get(consts.getContracts, {
         offset: offset, limit: limit
--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -5,7 +5,8 @@ import {useNavigate} from 'react-router-dom';
 import {Controller, form, useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 // @mui
-import {Stack, IconButton, InputAdornment, Button, TextField} from '@mui/material';
+import {Stack, IconButton, InputAdornment, Button, TextField, Alert} from '@mui/material';
+import Snackbar from "@mui/material/Snackbar";
 // components
 import Iconify from '../../../components/Iconify';
 import * as API from "../../../constants";
@@ -14,6 +15,9 @@ import * as API from "../../../constants";
 
 export default function LoginForm() {
     const navigate = useNavigate();
+    const [isOpen, setOpen] = useState(false);
+    const [message, setMessage] = useState(false);
+    const [type, setType] = useState("error");
 
     const [showPassword, setShowPassword] = useState(false);
 
@@ -33,7 +37,15 @@ export default function LoginForm() {
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
             }
-        }).then((res) => res.json())
+        }).then((res) => {
+            if (res.status !== 200) {
+                setMessage("Login fail")
+                setOpen(true);
+                setType("error")
+            } else {
+                return res.json()
+            }
+        })
             .then((res) => {
                 localStorage.setItem("ACCESS_TOKEN", res.access_token);
                 navigate('/dashboard/app')
@@ -45,46 +57,60 @@ export default function LoginForm() {
     });
 
     return (
-        <form>
-            <Stack spacing={3}>
-                <Controller
-                    name="username"
-                    control={control}
-                    render={({field: {onChange, value}}) => (
-                        <TextField fullWidth onChange={onChange} value={value} label="Input username"
-                                   error={Boolean(errors.username)}
-                                   helperText={errors.username?.message}/>
-                    )}
-                />
+        <>
+            <form>
+                <Stack spacing={3}>
+                    <Controller
+                        name="username"
+                        control={control}
+                        render={({field: {onChange, value}}) => (
+                            <TextField fullWidth onChange={onChange} value={value} label="Input username"
+                                       error={Boolean(errors.username)}
+                                       helperText={errors.username?.message}/>
+                        )}
+                    />
 
-                <Controller
-                    name="password"
-                    control={control}
-                    render={({field: {onChange, value}}) => (
-                        <TextField fullWidth
-                                   onChange={onChange}
-                                   value={value}
-                                   label="Input email"
-                                   type={showPassword ? 'text' : 'password'}
-                                   error={Boolean(errors.password)}
-                                   helperText={errors.password?.message}
-                                   InputProps={{
-                                       endAdornment: (
-                                           <InputAdornment position="end">
-                                               <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                                                   <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}/>
-                                               </IconButton>
-                                           </InputAdornment>
-                                       ),
-                                   }}
-                        />
-                    )}
-                />
-            </Stack>
+                    <Controller
+                        name="password"
+                        control={control}
+                        render={({field: {onChange, value}}) => (
+                            <TextField fullWidth
+                                       onChange={onChange}
+                                       value={value}
+                                       label="Input email"
+                                       type={showPassword ? 'text' : 'password'}
+                                       error={Boolean(errors.password)}
+                                       helperText={errors.password?.message}
+                                       InputProps={{
+                                           endAdornment: (
+                                               <InputAdornment position="end">
+                                                   <IconButton onClick={() => setShowPassword(!showPassword)}
+                                                               edge="end">
+                                                       <Iconify
+                                                           icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'}/>
+                                                   </IconButton>
+                                               </InputAdornment>
+                                           ),
+                                       }}
+                            />
+                        )}
+                    />
+                </Stack>
 
-            <Button fullWidth size="large" type="submit" variant="contained" sx={{mt: 2}} onClick={handleSubmit(onSubmit)}>
-                Login
-            </Button>
-        </form>
+                <Button fullWidth size="large" type="submit" variant="contained" sx={{mt: 2}}
+                        onClick={handleSubmit(onSubmit)}>
+                    Login
+                </Button>
+            </form>
+
+            <Snackbar sx={{
+                width: '40%', position: 'absolute', top: '0%', left: '0%',
+            }} open={isOpen} autoHideDuration={6000} onClose={() => setOpen(false)}>
+                <Alert onClose={() => setOpen(false)} severity={type}
+                       sx={{width: '100%'}}>
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
     );
 }
