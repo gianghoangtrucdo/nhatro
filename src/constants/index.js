15ecaadfd40409480e8925884c9cd45492f14df5
Tourist <tourisversion@gmail.com>
2022-07-21 15:54:47 +0700

    Add connector

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ const serverAddress = `${process.env.REACT_APP_SERVER_NAME}`;
 // api constant
 module.exports = Object.freeze({
     users: `${serverAddress}/user/list?offset=0&limit=100`,
+    getHosts: `${serverAddress}/user/host`,
     createUser: `${serverAddress}/user/create`,
     updateUser: `${serverAddress}/user/update`
 });
